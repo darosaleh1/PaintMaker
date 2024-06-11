c633e4dc5b6c1e1d66f74fdb9d2f27c7aba7ddd7
darosaleh <darosaleh@gmail.com>
2024-06-11 11:30:49 +0100

    main functionality complete

--- index.js
+++ index.js
@@ -10,14 +10,39 @@ const askNumberOfRooms = async () => {
     return numberOfRooms.rooms;
 };
 const askNumberOfWalls = async () => {
-    const numberOfWalls = await inquirer.prompt([
+    const walls = await inquirer.prompt([
         {
             type: 'number',
             name: 'walls',
             message: 'How many walls are you painting in this room?'
         }
     ]);
-    return numberOfWalls.walls;
+    return walls.walls;
+};
+const askHowManyCoats = async () => {
+    const coats = await inquirer.prompt([
+        {
+            type: 'number',
+            name: 'coats',
+            message: 'How many coats of paint do you need for this wall?'
+        }
+    ]);
+    return coats.coats;
+};
+const askBrand = async () => {
+    const paintBrand = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'brand',
+            choices: [
+                'Farrow & Ball',
+                'Dulux',
+                'Lick',
+            ],
+            message: 'Which brand would you like for this colour? (ordered by price)'
+        }
+    ]);
+    return paintBrand.brand;
 };
 function calculateArea(width, height) {
     return width * height;
@@ -60,46 +85,111 @@ const whichColour = async () => {
             choices: [
                 'White',
                 'Cream',
-                'Sky Blue',
                 'Navy Blue',
                 'Light Grey',
-                'Steel Grey',
                 'Baby Pink',
-                'Blush Pink',
                 'Sage Green',
-                'Teal'
             ],
+            message: 'Which colour would you like to paint this wall?'
         }
     ]);
     return room_colour.colour;
 };
-let map = new Map();
-map.set('White', 10);
-map.set('Cream', 15);
-map.set('Sky Blue', 10);
-map.set('Navy Blue', 20);
-map.set('Light Grey', 5);
-map.set('Steel Grey', 15);
-map.set('Baby Pink', 10);
-map.set('Blush Pink', 25);
-map.set('Sage Green', 15);
-map.set('Teal', 20);
+function minimumPaintCombination(paintNeeded) {
+    const bucketSizes = [10, 5, 2.5, 1];
+    let paintAmountMap = new Map([
+        [10, 0],
+        [5, 0],
+        [2.5, 0],
+        [1, 0],
+    ]);
+    paintNeeded = Math.ceil(paintNeeded);
+    for (let bucketSize of bucketSizes) {
+        if (paintNeeded >= bucketSize) {
+            let numberOfBuckets = Math.floor(paintNeeded / bucketSize);
+            paintNeeded -= (numberOfBuckets * bucketSize);
+            paintAmountMap.set(bucketSize, (paintAmountMap.get(bucketSize) || 0) + numberOfBuckets);
+        }
+    }
+    return paintAmountMap;
+}
+let fAndBMap = new Map([
+    ['White', [8, 16, 30, 48]],
+    ['Cream', [14, 40, 70, 136]],
+    ['Navy Blue', [24, 60, 110, 168]],
+    ['Light Grey', [24, 60, 110, 168]],
+    ['Baby Pink', [24, 60, 110, 168]],
+    ['Sage Green', [24, 60, 110, 168]],
+]);
+let duluxMap = new Map([
+    ['White', [4, 8, 15, 24]],
+    ['Cream', [7, 20, 35, 68]],
+    ['Navy Blue', [12, 30, 55, 84]],
+    ['Light Grey', [12, 30, 55, 84]],
+    ['Baby Pink', [12, 30, 55, 84]],
+    ['Sage Green', [15, 30, 55, 84]],
+]);
+let lickMap = new Map([
+    ['White', [3, 6, 11, 21]],
+    ['Cream', [5, 14, 26, 50]],
+    ['Navy Blue', [9, 25, 48, 80]],
+    ['Light Grey', [9, 25, 48, 80]],
+    ['Baby Pink', [9, 25, 48, 80]],
+    ['Sage Green', [9, 25, 48, 80]],
+]);
+let paintBrandsMap = new Map([
+    ['Farrow & Ball', fAndBMap],
+    ['Dulux', duluxMap],
+    ['Lick', lickMap],
+]);
+let paintNeededMap = new Map([
+    ['White', 0],
+    ['Cream', 0],
+    ['Navy Blue', 0],
+    ['Light Grey', 0],
+    ['Baby Pink', 0],
+    ['Sage Green', 0]
+]);
+function updatePaintNeeded(roomColour, paintNeeded) {
+    const currentValue = paintNeededMap.get(roomColour);
+    paintNeededMap.set(roomColour, (currentValue || 0) + paintNeeded);
+}
 async function main() {
     const numberOfRooms = await askNumberOfRooms();
     console.log(`You are painting ${numberOfRooms} rooms!`);
-    let roomPaintList = [];
     for (let i = 0; i < numberOfRooms; i++) {
         let numberOfWalls = await askNumberOfWalls();
         console.log(`You are painting ${numberOfWalls} walls!`);
-        let wallArea = 0;
         for (let j = 0; j < numberOfWalls; j++) {
+            console.log(`Wall ${j + 1}:`);
+            let wallArea = 0;
             wallArea += await calculateIncludedWallArea();
             wallArea -= await calculateExcludedWallArea();
+            let coats = await askHowManyCoats();
+            let roomColour = await whichColour();
+            let paintNeeded = parseFloat(((wallArea / 10) * coats).toFixed(2));
+            updatePaintNeeded(roomColour, paintNeeded);
         }
-        let roomColour = await whichColour();
-        let roomTuple = [wallArea, roomColour];
-        roomPaintList.push(roomTuple);
-        console.log(`You are painting room ${i + 1} in ${roomTuple[1]}, this will cost £${(map.get(roomTuple[1]) * roomTuple[0])}`);
     }
+    let total = 0;
+    for (let [key, value] of paintNeededMap) {
+        if (value > 0) {
+            let brand = await askBrand();
+            console.log(`You need ${value} litres of ${key} paint of the brand ${brand}`);
+            let amounts = await minimumPaintCombination(value);
+            let paintBrandPrices = paintBrandsMap.get(brand)?.get(key)?.reverse();
+            let idx = 0;
+            for (let [key_1, value_1] of amounts) {
+                if (value_1 > 0) {
+                    const bucketPrice = paintBrandPrices?.[idx];
+                    if (bucketPrice !== undefined) {
+                        console.log(`For the ${key} paint, you need ${value_1} buckets of ${key_1} litres, this costs £${bucketPrice * value_1}`);
+                        total += (bucketPrice * value_1);
+                    }
+                }
+            }
+        }
+    }
+    console.log(`Your total cost is £${total}`);
 }
 main();
--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ const askNumberOfRooms = async (): Promise<number> => {
     
      const askNumberOfWalls = async (): Promise<number> => {
 
-        const numberOfWalls = await inquirer.prompt([
+        const walls = await inquirer.prompt([
             {
                 type: 'number',
                 name: 'walls',
@@ -24,10 +24,43 @@ const askNumberOfRooms = async (): Promise<number> => {
             }
         ])
 
-        return numberOfWalls.walls
+        return walls.walls
 
      }
 
+     const askHowManyCoats = async (): Promise<number> => {
+
+        const coats = await inquirer.prompt([
+            {
+                type: 'number',
+                name: 'coats',
+                message: 'How many coats of paint do you need for this wall?'
+    
+            }
+        ])
+
+        return coats.coats
+    }
+
+     const askBrand = async(): Promise<string> => {
+
+        const paintBrand = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'brand',
+                choices: [
+                    'Farrow & Ball',
+                    'Dulux',
+                    'Lick',
+                    
+                ],
+                message: 'Which brand would you like for this colour? (ordered by price)'
+            }
+    
+        ])
+        return paintBrand.brand
+    
+    }
 function calculateArea(width:number, height:number): number {
     return width * height
 }
@@ -84,15 +117,12 @@ const whichColour = async(): Promise<string> => {
             choices: [
                 'White',
                 'Cream',
-                'Sky Blue',
                 'Navy Blue',
                 'Light Grey',
-                'Steel Grey',
                 'Baby Pink',
-                'Blush Pink',
                 'Sage Green',
-                'Teal'
             ],
+            message: 'Which colour would you like to paint this wall?'
         }
 
     ])
@@ -100,47 +130,148 @@ const whichColour = async(): Promise<string> => {
 
 }
 
-let map = new Map()
-map.set('White',10)
-map.set('Cream',15)
-map.set('Sky Blue',10)
-map.set('Navy Blue',20)
-map.set('Light Grey',5)
-map.set('Steel Grey',15)
-map.set('Baby Pink',10)
-map.set('Blush Pink',25)
-map.set('Sage Green',15)
-map.set('Teal',20)
+
+function minimumPaintCombination(paintNeeded:number): Map<number,number> {
+    const bucketSizes = [10,5,2.5,1];
+
+    let paintAmountMap = new Map<number,number>([
+        [10,0],
+        [5,0],
+        [2.5,0],
+        [1,0],
+    ]);
+
+    paintNeeded = Math.ceil(paintNeeded)
+
+    for(let bucketSize of bucketSizes){
+        if (paintNeeded >= bucketSize){
+            let numberOfBuckets = Math.floor(paintNeeded / bucketSize);
+            paintNeeded -= (numberOfBuckets * bucketSize)
+            paintAmountMap.set(bucketSize, (paintAmountMap.get(bucketSize) ||0) + numberOfBuckets);
+        }
+    }
+    return paintAmountMap
+    
+}
+
+
+
+// dont forget daro - todo list: fix remaining logic should take an hour tops
+// then add error handling and then finish by adding testing! 9-2pm should be plenty of time lock in boy
+
+// idea: use different map for each brand + simplify down to 5 colours
+
+type PaintMap = Map<string, number[]>;
+
+let fAndBMap: PaintMap = new Map([
+    ['White', [8, 16,30,48]],
+    ['Cream', [14, 40, 70, 136]],
+    ['Navy Blue', [24, 60,110,168]],
+    ['Light Grey', [24, 60,110,168]],
+    ['Baby Pink', [24, 60,110,168]],
+    ['Sage Green', [24, 60,110,168]],
+]);
+
+let duluxMap: PaintMap = new Map([
+    ['White', [4, 8,15, 24]],
+    ['Cream', [7, 20, 35, 68]],
+    ['Navy Blue', [12, 30,55,84]],
+    ['Light Grey', [12, 30,55,84]],
+    ['Baby Pink', [12,30,55,84]],
+    ['Sage Green', [15,30,55, 84]],
+]);
+
+
+
+let lickMap: PaintMap = new Map([
+    ['White', [3, 6,11, 21]],
+    ['Cream', [5, 14, 26, 50]],
+    ['Navy Blue', [9, 25,48,80]],
+    ['Light Grey', [9, 25,48,80]],
+    ['Baby Pink', [9, 25,48,80]],
+    ['Sage Green', [9, 25,48,80]],
+]);
+
+type PaintBrandMap = Map<string,PaintMap>;
+
+let paintBrandsMap: PaintBrandMap = new Map([
+    ['Farrow & Ball', fAndBMap],
+    ['Dulux', duluxMap],
+    ['Lick', lickMap],
+])
+
+
+let paintNeededMap: Map<string, number> = new Map([
+    ['White',0],
+    ['Cream',0],
+    ['Navy Blue',0],
+    ['Light Grey',0],
+    ['Baby Pink',0],
+    ['Sage Green',0]
+])
+
+function updatePaintNeeded(roomColour:string, paintNeeded: number): void{
+    const currentValue = paintNeededMap.get(roomColour);
+            paintNeededMap.set(roomColour, (currentValue || 0) + paintNeeded)
+}
 
 
 
 
 async function main() {
 
-    
-    const numberOfRooms = await askNumberOfRooms()
+    const numberOfRooms: number = await askNumberOfRooms()
     console.log(`You are painting ${numberOfRooms} rooms!`)
 
     
-    let roomPaintList: [number,string][] = []
 
     for (let i = 0; i < numberOfRooms; i++){
 
-        let numberOfWalls = await askNumberOfWalls()
+        let numberOfWalls: number = await askNumberOfWalls()
         console.log(`You are painting ${numberOfWalls} walls!`)
-        let wallArea: number = 0
 
 
         for (let j = 0; j < numberOfWalls; j++) {
+            console.log(`Wall ${j+1}:`)
+            let wallArea: number = 0
             wallArea += await calculateIncludedWallArea()
             wallArea -= await calculateExcludedWallArea()
+
+            let coats: number = await askHowManyCoats()
+            let roomColour: string = await whichColour()
+            let paintNeeded: number = parseFloat(((wallArea / 10) * coats).toFixed(2));
+            updatePaintNeeded(roomColour,paintNeeded)
+
+            
+
             
         }
-        let roomColour = await whichColour()
-        let roomTuple: [number,string] = [wallArea,roomColour]
-        roomPaintList.push(roomTuple)
-        console.log(`You are painting room ${i+1} in ${roomTuple[1]}, this will cost £${(map.get(roomTuple[1])*roomTuple[0])}`)
         }
+    
+        let total: number = 0
+        for (let [key,value] of paintNeededMap){
+            if (value > 0){
+                let brand: string = await askBrand()
+
+                console.log(`You need ${value} litres of ${key} paint of the brand ${brand}`)
+                let amounts: Map<number,number> = await minimumPaintCombination(value)
+                let paintBrandPrices: number[] | undefined = paintBrandsMap.get(brand)?.get(key)?.reverse()
+                let idx:number = 0
+                for (let [key_1,value_1] of amounts) {
+                    if (value_1 > 0){
+                    const bucketPrice: number | undefined = paintBrandPrices?.[idx]
+                    if (bucketPrice !== undefined){
+                    console.log(`For the ${key} paint, you need ${value_1} buckets of ${key_1} litres, this costs £${bucketPrice*value_1}`)
+                    total += (bucketPrice*value_1)
+                    }
+                    }
+
+                }
+    
+            }
+        }
+        console.log(`Your total cost is £${total}`)
+
 
     
 
